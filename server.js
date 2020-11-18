f59115f585e331f9d216a37578a3a1451acd3bab
Tanvir <34381989+tshahria@users.noreply.github.com>
2020-11-18 17:47:30 -0500

    adding in mongoose/mongodb

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose");
 
+// connecting to mongo db
+const uri = "mongodb+srv://master:cs1300capstone@cluster0.b5rtk.mongodb.net/main?retryWrites=true&w=majority";
+connectToDB = async () => {
+    await mongoose.connect(uri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+            useCreateIndex: true
+        });
+    // console.log(mongoose.connection.readyState);
+};
+connectToDB();
+
+// server
 const app = express();
 
 app.use(express.static(path.join(__dirname, "/build")));
