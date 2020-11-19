9dba0d431a8a7832312ef7e4c34228113c334a46
Tanvir <34381989+tshahria@users.noreply.github.com>
2020-11-19 14:56:53 -0500

    add in constant for game data

---
+++ src/gameData.js
@@ -0,0 +1,16 @@
+export const gameData = {
+    1 : {
+        "Name" : ,
+        "Price": ,
+        "Day": ,
+        "Time": ,
+        "Difficulty": ,
+        "DateTime": ,
+        "GameType": ,
+        "Logo": 
+    },
+    
+    2 : {
+
+    }
+}
