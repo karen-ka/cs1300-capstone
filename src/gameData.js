e066c74e6e0cba7ac92210bf2066ca796bf1f9fa
Tanvir <34381989+tshahria@users.noreply.github.com>
2020-11-19 19:54:32 -0500

    added images, and more data

--- src/gameData.js
+++ src/gameData.js
@@ -1,16 +1,42 @@
 export const gameData = {
     1 : {
-        "Name" : ,
-        "Price": ,
-        "Day": ,
-        "Time": ,
-        "Difficulty": ,
-        "DateTime": ,
-        "GameType": ,
-        "Logo": 
+        "Name" : "Beginner DnD",
+        "Price": 20,
+        "Day": "Monday/Wednesday",
+        "Time": "9P.M",
+        "Difficulty": "Easy",
+        "Information": "Welcome to DnD! This game is suitable for first-time players or those wanting to brush up their dragon slaying skills."
+        "Host" : "Alex"
+        "DateTime": "",
+        "GameType": "DnD",
+        "Logo": './game_logos/dnd.png',
+        "Location": "Zoom"
     },
-    
-    2 : {
 
-    }
+    2 : {
+        "Name" : "Intermediate Magic: the Gatering",
+        "Price": 10,
+        "Day": "Tuesday/Thursday",
+        "Time": "6P.M",
+        "Difficulty": "Medium",
+        "Information": "Welcome to Magic! This game is suitable for intermediate players. We allow all decks/expansions."
+        "Host" : "Brian"
+        "DateTime": "",
+        "GameType": "Magic",
+        "Logo": './game_logos/magic.jpg',
+        "Location": "Zoom"
+    },
+    3 : {
+        "Name" : "Advanced Magic: the Gatering",
+        "Price": 25,
+        "Day": "Tuesday/Saturday",
+        "Time": "2P.M",
+        "Difficulty": "Medium",
+        "Information": "This game is suitable for advanced players. We allow all decks/expansions. "
+        "Host" : "Tanvir"
+        "DateTime": "",
+        "GameType": "Magic",
+        "Logo": './game_logos/magic.jpg',
+        "Location": "Zoom"
+    },
 }
