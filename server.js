b60d6680fae27ed21c72fa39c01120f2aeb600b0
Patrick Zhang <patrick_zhang1@brown.edu>
2021-01-12 21:37:07 -0500

    Prep for deployment

--- server.js
+++ server.js
@@ -110,6 +110,7 @@ app.get("*", (_, res) => {
     res.sendFile(path.join(__dirname + "/build/index.html"));
 });
 
-app.listen(8080, () => {
-    console.log("Server started on port 8080");
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
 });
